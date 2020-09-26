2e5dd26cff5fa019b256a8a663a93e1f9dadf746
Rohan <rohanharris1@outlook.com>
2020-09-26 22:26:39 +0100

    minor changes made to code structure in form.css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,34 +7,6 @@ import Form from '../Form';
 import Match from '../Match';
 
 
-// import { createStore } from 'redux';
-
-// const initialState = {
-//     players: [],
-//     name: "",
-//     error: false,
-
-// };
-
-
-
-// added to store.js
-
-// const store = createStore(
-  // reducer, 
-  // initialState,
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-
-  // );
-
-
-// dont need this. 
-// store.suscribe(() => {
-//   console.log(store.getState());
-// });
-
-// store.dispatch({ type: "NEWPLAYER" })
-
 const App = ({ tournament }) => (
 
   <>
@@ -45,27 +17,5 @@ const App = ({ tournament }) => (
 
 );
 
-
-
-// function App() {
-//   return (
-//     <React.Fragment>
-//       <Router>
-//         <Header />
-//         <Switch>
-
-//           <Route exact path ="/">
-//            <Form />
-//           </Route>
-
-//           <Route exact path="/match">
-//             <Match />
-//           </Route>
-
-//         </Switch>
-//     </Router>
-//     </React.Fragment>
-//   );
-// }
 
 export default App;
