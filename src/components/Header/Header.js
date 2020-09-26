390cd451f7591da3378af4e543a9e645dd23fedf
Rohan <rohanharris1@outlook.com>
2020-09-26 22:12:26 +0100

    media queries added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Header = () => (
 <>
 <header className='header'>
-    {/* <h1> Lord of the Pings </h1> */}
+    <h1 className='hidden'> Lord of the Pings </h1>
     {/* <a href="https://fontmeme.com/the-lord-of-the-rings-font/"> */}
     <img src="https://fontmeme.com/permalink/200926/2575f5be0f48e580570ac8b0c2f2e6ed.png" alt="the-lord-of-the-rings-font" border="0"/>
 
