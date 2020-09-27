9b5f49b88e936f222439b8884211fb0eed33822c
Rohan <rohanharris1@outlook.com>
2020-09-27 10:50:47 +0100

    Updates

---
+++ precache-manifest.d5a8f78cf092634444a8cee0566599ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c6465fda1d405fa74b283ad988200c",
+    "url": "/ping-pong-app/index.html"
+  },
+  {
+    "revision": "69699cedb23703ac7afc",
+    "url": "/ping-pong-app/static/css/main.855619e1.chunk.css"
+  },
+  {
+    "revision": "d1deee5e16ad28d38e72",
+    "url": "/ping-pong-app/static/js/2.477444db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ping-pong-app/static/js/2.477444db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69699cedb23703ac7afc",
+    "url": "/ping-pong-app/static/js/main.47975dbe.chunk.js"
+  },
+  {
+    "revision": "10d2b714630890362419",
+    "url": "/ping-pong-app/static/js/runtime-main.9ff82a9c.js"
+  }
+]);
